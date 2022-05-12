12142b8d260c95188428af6f0cedf50e5076312e
Alan Berberov <alan.almaco67@gmail.com>
2022-05-12 02:03:34 +0200

    Fixed visual: - Particles - Trail;

--- Physics/Assets/Scripts/gravityChange.cs
+++ Physics/Assets/Scripts/gravityChange.cs
@@ -4,23 +4,33 @@ using UnityEngine;
 public class gravityChange : MonoBehaviour
 {
 	[SerializeField] private PlayerController _controller;
+	[SerializeField] private TrailRenderer _trail;
+	[SerializeField] private GameObject _particles;
 
 	private void Update()
 	{
 		if (Input.GetKeyDown(KeyCode.O) && cameraRotation.CanSwitchGravity)
 		{
-			transform.eulerAngles += Vector3.forward * 90f;
+			_particles.SetActive(false);
+			transform.eulerAngles += Vector3.forward * 90f;			
 
 			var t = _controller.transform.position;
 			_controller.transform.position = new Vector3(-t.y, t.x, 0f);
+			
+			_trail.Clear();
+			_particles.SetActive(true);
 		}
 
 		if (Input.GetKeyDown(KeyCode.P) && cameraRotation.CanSwitchGravity)
 		{
+			_particles.SetActive(false);
 			transform.eulerAngles += Vector3.back * 90f;
 
 			var t = _controller.transform.position;
 			_controller.transform.position = new Vector3(t.y, -t.x, 0f);
+
+			_trail.Clear();
+			_particles.SetActive(true);
 		}
 	}
 }
